63b8690e062dffb70e55ad1ac40113cf73d142e1
João Guilherme <souza.joaoguilherme2002@gmail.com>
2021-01-21 22:36:36 -0300

    Deleção de usuário com DELETE

--- src/main/java/com/gui/workshopmongo/resource/UserResource.java
+++ src/main/java/com/gui/workshopmongo/resource/UserResource.java
@@ -49,4 +49,13 @@ public class UserResource {
 		
 		return ResponseEntity.created(uri).build();
 	}
+	
+	
+	@RequestMapping(method=RequestMethod.DELETE, value="/{id}")
+	public ResponseEntity<Void> delete(@PathVariable String id) {
+		
+		service.delete(id);
+		
+		return ResponseEntity.noContent().build();
+	}
 }
--- src/main/java/com/gui/workshopmongo/service/UserService.java
+++ src/main/java/com/gui/workshopmongo/service/UserService.java
@@ -40,6 +40,12 @@ public class UserService {
 		return repo.insert(obj);
 	}
 	
+	public void delete(String id) {
+		this.findById(id);
+		repo.deleteById(id);
+	}
+	
+	
 	public User fromDTO(UserDTO objDto) {
 		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
