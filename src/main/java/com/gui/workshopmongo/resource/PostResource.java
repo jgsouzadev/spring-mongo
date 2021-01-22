bd92392cc8cdc0f7f99ba19d58ef6fa7038dc6a1
João Guilherme <souza.joaoguilherme2002@gmail.com>
2021-01-22 15:25:15 -0300

    Consultas simples com query methods

--- src/main/java/com/gui/workshopmongo/resource/PostResource.java
+++ src/main/java/com/gui/workshopmongo/resource/PostResource.java
@@ -1,12 +1,16 @@
 package com.gui.workshopmongo.resource;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.gui.workshopmongo.domain.Post;
+import com.gui.workshopmongo.resource.util.URL;
 import com.gui.workshopmongo.service.PostService;
 
 @RestController
@@ -23,6 +27,13 @@ public class PostResource {
 		Post postObj = service.findById(id);
 		
 		return ResponseEntity.ok().body(postObj);
+	}
+	
+	@RequestMapping(method=RequestMethod.GET, value="/titlesearch")
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue="") String text) {
+		text = URL.decodeParam(text);
+		List<Post> list = service.findByTitle(text);
+		return ResponseEntity.ok().body(list);
 	}
 	
 	
--- src/main/java/com/gui/workshopmongo/service/PostService.java
+++ src/main/java/com/gui/workshopmongo/service/PostService.java
@@ -1,5 +1,6 @@
 package com.gui.workshopmongo.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,4 +29,8 @@ public class PostService {
 		return post;
 	}
 
+	
+	public List<Post> findByTitle(String text) {
+		return repo.findByTitleContainingIgnoreCase(text);
+	}
 }
